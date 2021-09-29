87d5c7657bab3d4c4ee124650418e7aedd58b80b
seohl16 <selim@student.42seoul.kr>
2021-09-29 17:56:49 +0900

    timescript show update and delete function added

--- board/public/javascript/timescript.js
+++ board/public/javascript/timescript.js
@@ -1,40 +1,40 @@
-//public/javascript/timescript.js
+// public/js/script.js
 
 $(function(){
-	function get2digits (num) {
-		return ('0' + num).slice(-2);
-	}
+  function get2digits (num){
+    return ('0' + num).slice(-2);
+  }
 
-	function getDate(dateObj){
-		if (dateObj instanceof Date)
-			return dateObj.getFullYear() + '-' + get2digits(dateObj.getMonth()+1) + '-' + get2digits(dateObj.getDate());
-	}
+  function getDate(dateObj){
+    if(dateObj instanceof Date)
+      return dateObj.getFullYear() + '-' + get2digits(dateObj.getMonth()+1)+ '-' + get2digits(dateObj.getDate());
+  }
 
-	function getTime(dateObj){
-		if (dateObj instanceof Date)
-			return get2digits(dateObj.getHours()) + ":" + get2digits(dateObj.getMinutes()) + ":" + get2digits(dateObj.getSeconds());
-	}
+  function getTime(dateObj){
+    if(dateObj instanceof Date)
+      return get2digits(dateObj.getHours()) + ':' + get2digits(dateObj.getMinutes())+ ':' + get2digits(dateObj.getSeconds());
+  }
 
-	function convertDate(){
-		$('[date-date]').each(function(index, element){
-			var dateString = $(element).date('date');
-			if (dateString){
-				var date = new Date(dateString);
-				$(element).html(getDate(date));
-			}
-		});
-	}
+  function convertDate(){
+    $('[data-date]').each(function(index,element){
+      var dateString = $(element).data('date');
+      if(dateString){
+        var date = new Date(dateString);
+        $(element).html(getDate(date));
+      }
+    });
+  }
 
-	function converDateTime() {
-		$('[data-date-time]').each(function(index, element){
-			var dateString = $(element).data('date-time');
-			if (dateString){
-				var date = new Date(dateString);
-				$(element).html(getDate(date)+' '+getTime(date));
-			}
-		});
-	}
+  function convertDateTime(){
+    $('[data-date-time]').each(function(index,element){
+      var dateString = $(element).data('date-time');
+      if(dateString){
+        var date = new Date(dateString);
+        $(element).html(getDate(date)+' '+getTime(date));
+      }
+    });
+  }
 
-	converDate();
-	converDateTime();
+  convertDate();
+  convertDateTime();
 });
--- board/routes/posts.js
+++ board/routes/posts.js
@@ -25,4 +25,37 @@ router.post('/', function (req, res) {
 	});
 });
 
+// show
+router.get('/:id', function(req, res){
+	Post.findOne({_id:req.params.id}, function(err, post){
+		if (err) return res.json(err);
+		res.render('posts/show', {post:post});
+	});
+})
+
+// edit
+router.get('/:id/edit', function (req, res) {
+	Post.findOne({_id:req.params.id}, function (err, result){
+		if (err) return res.json(err);
+		res.render('posts/edit', {post:result})
+	})
+})
+
+// update
+router.put('/:id', function (req, res){
+	req.body.updatedAt = Date.now();
+	Post.findOneAndUpdate({_id:req.params.id}, req.body, function (err, result){
+		if (err) return (res.json(err));
+		res.redirect("/posts/"+req.params.id);
+	})
+})
+
+//delete
+router.delete('/:id', function(req, res){
+	Post.deleteOne({_id:req.params.id}, function (err, result){
+		if (err) return res.json(err);
+		res.redirect('/posts');
+	})
+})
+
 module.exports = router;
